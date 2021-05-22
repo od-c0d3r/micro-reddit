f6e8a78ac68811465f022cbee61157342011e4f7
rashad <omar_rashad@aol.com>
2021-05-22 13:17:54 +0200

    Creating new miration file, adding email column with corsbonding validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: { maximum: 30 }
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 
   has_many :posts
   has_many :comments
